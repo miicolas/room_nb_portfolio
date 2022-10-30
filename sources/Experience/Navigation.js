ed4716a715c3cf80f8f3ccec771e92b8621cc32f
Nicolas Becharat <78233186+justniicolas@users.noreply.github.com>
2022-10-30 10:50:26 +0100

    Update Navigation.js

--- sources/Experience/Navigation.js
+++ sources/Experience/Navigation.js
@@ -1,8 +1,6 @@
 import * as THREE from 'three'
 import Experience from './Experience.js'
-import normalizeWheel from 'normalize-wheel'
-import World from './World.js'
-import { Float, Environment, useGLTF, OrbitControls } from '@react-three/drei'
+
 
 
 export default class Navigation
@@ -10,10 +8,9 @@ export default class Navigation
     constructor()
     {
         this.experience = new Experience()
-        this.targetElement = this.experience.targetElement
+
         this.camera = this.experience.camera
-        this.config = this.experience.config
-        this.time = this.experience.time
+
 
         this.setView()
     }
@@ -26,8 +23,7 @@ export default class Navigation
 
         this.view.spherical = {}
         this.view.spherical.value = new THREE.Spherical(30, Math.PI * 0.35, - Math.PI * 0.25)
-        // this.view.spherical.value.radius = 5
-        this.view.spherical.smoothed = this.view.spherical.value.clone()
+        console.log (this.view.spherical.value)
 
 
         
